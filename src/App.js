b7b1a68791b2ffad498efd1b3ec03a6688b81d51
Sam Tomashi <samtomashikayembi@yahoo.fr>
2024-11-04 10:44:27 +0300

    forms and controlled inputs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./Bootstrap.css";
+import "./App.css";
+import React from "react";
 
 function App() {
+
+  const [formData, setFormData] =React.useState({
+    email: "",
+    password: ""
+  })
+
+  function handleOnchange(event) {
+      setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  function handleOnSubmit(event) {
+    event.preventDefault();
+     if(isEmailValid(formData.email) && isPasswordValid(formData.password)){
+          console.log(formData);
+    }else{
+      console.log("Email or password is not valid")
+    }
+  }
+
+  function isEmailValid(email) {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+  }
+
+  function isPasswordValid(password) {
+    const regex = /^.{8,}$/;
+    return regex.test(password);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleOnSubmit} className="p-4">
+      <p>email</p>
+      <input type="text" name="email" onChange={handleOnchange} />
+      <br />
+      <p>Password</p>
+      <input type="password" name="password" onChange={handleOnchange} />
+      <br />
+      <button type="submit">Login</button>
+    </form>
   );
 }
 
